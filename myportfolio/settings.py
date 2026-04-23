e5d59e4050a07bc10cf632be60ecdb3d9d24a56a
dipakbhatt-09 <bhattdipak90@gmail.com>
2026-04-23 15:58:54 +0545

    fix dj-database-url and settings update

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -76,19 +76,6 @@ WSGI_APPLICATION = 'myportfolio.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/6.0/ref/settings/#databases
-
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'mydb_0ob6',
-#         'USER': 'mydb_0ob6_user',
-#         'PASSWORD': 'EdG0jeCbGYHAcGuWAmPEvmnHk9DgTYqE',
-#         'HOST': 'dpg-d7kbkf3bc2fs73809u50-a',
-#         'PORT': '5432',
-#     }
-# }
 import dj_database_url
 
 DATABASES = {
@@ -100,15 +87,6 @@ DATABASES = {
     )
 }
 
-
-
-# import dj_database_url
-
-# DATABASES = {
-#     'default': dj_database_url.parse(
-#         "postgresql://mydb_0ob6_user:EdG0jeCbGYHAcGuWAmPEvmnHk9DgTYqE@dpg-d7kbkf3bc2fs73809u50-a.oregon-postgres.render.com/mydb_0ob6"
-#     )
-# }
 
 
 # Password validation
