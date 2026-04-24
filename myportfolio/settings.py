2e4816b50f488af7df5ff2b51019b5d0e8f30786
dipakbhatt-09 <bhattdipak90@gmail.com>
2026-04-24 13:25:35 +0545

    fix python version to 3.11

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -80,11 +80,13 @@ import dj_database_url
 
 
 
+import dj_database_url
+
 DATABASES = {
     'default': dj_database_url.config(
-        default=os.environ.get("DATABASE_URL", ""),
+        default='sqlite:///db.sqlite3',
         conn_max_age=600,
-        ssl_require=True
+        ssl_require=False
     )
 }
 
